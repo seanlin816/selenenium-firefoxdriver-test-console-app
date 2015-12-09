f6c43e83ab855653b69c53157f7ed5878d2525fe
Sean Lin <seanlin816@gmail.com>
2015-12-09 10:51:37 +0800

    no message

--- Program.cs
+++ Program.cs
@@ -9,38 +9,9 @@ namespace FirefoxDriverConsoleApp
     {
         static void Main()
         {
-            using (var driver = new FirefoxDriver())
-            {
-                driver.Navigate().GoToUrl("https://www.google.fr");
-
-                var waiter = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
-
-                // Get google input text element by name
-                var elementSelector = By.Name("q");
-
-                // Wait until this element is visible
-                waiter.Until(ExpectedConditions.ElementIsVisible(elementSelector));
-
-                // Get element
-                var element = driver.FindElement(elementSelector);
-
-                // Fill in input text
-                element.SendKeys("test");
-
-                // Submit page form
-                element.Submit();
-
-                // Wait until result stats are visible to get page title
-                var resultStatsElementSelector = By.Id("resultStats");
-                waiter.Until(ExpectedConditions.ElementIsVisible(resultStatsElementSelector));
-
-                // Get page title
-                var pageTitle = driver.Title;
-
-                Console.WriteLine("Page title: " + pageTitle);
-
-                driver.Quit();
-            }
+            SPGData spg = new SPGData();
+            string result = spg.GetDeactivationData(null);
+            Console.WriteLine(result);
         }
     }
 }
---
+++ SPGData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace FirefoxDriverConsoleApp
+{
+    public class SPGData
+    {
+        public string GetDeactivationData(string esn)
+        {
+            using (var driver = new FirefoxDriver())
+            {
+                string baseURL = "https://www.boostmobilesales.com/";
+                driver.Navigate().GoToUrl(baseURL + "/boost-sales-portal/faces/login.jsp");
+                driver.FindElement(By.Id("loginform:username")).Clear();
+                driver.FindElement(By.Id("loginform:username")).SendKeys("vha");
+                driver.FindElement(By.Id("loginform:password")).Clear();
+                driver.FindElement(By.Id("loginform:password")).SendKeys("1550Boost!");
+                driver.FindElement(By.Id("loginform:Login")).Click();
+
+                driver.Navigate().GoToUrl(baseURL + "boost-sales-portal/faces/_rlvid.jsp?_rap=pc_AccountPreferences.ESNLookup&_rvip=/accountPreferences.jsp");
+                driver.FindElement(By.Id("meid")).Clear();
+                driver.FindElement(By.Id("meid")).SendKeys("089446861501672553");
+                driver.FindElement(By.Id("form1:Search")).Click();
+                var result = driver.FindElement(By.XPath("//table[@id='dataTable']/tbody/tr[2]/td[4]")).Text;
+                Console.WriteLine("Result title: " + result);
+
+                driver.Quit();
+                return result;
+            }
+        }
+    }
+}
